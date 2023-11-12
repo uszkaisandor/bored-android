9862d93215f41f14b897cf0df0f0e6b92cf6fb2f
Sándor Uszkai <uszkaisanyi@gmail.com>
2023-11-12 15:49:29 +0100

    Setup Dagger Hilt and add custom application

---
+++ app/src/main/java/com/uszkaisandor/bored/vm/ActivitiesViewModel.kt
@@ -0,0 +1,8 @@
+package com.uszkaisandor.bored.vm
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ActivitiesViewModel : ViewModel() {
+}
