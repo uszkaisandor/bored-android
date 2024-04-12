9dadd6f64143990bce202140b0482ac27d6d55f3
Sándor Uszkai <uszkaisanyi@gmail.com>
2024-04-12 22:05:56 +0200

    Add empty state to favourites

---
+++ app/src/main/java/com/uszkaisandor/bored/presentation/favourites/FavouriteActivitiesList.kt
@@ -0,0 +1,57 @@
+package com.uszkaisandor.bored.presentation.favourites
+
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.itemKey
+import com.uszkaisandor.bored.domain.LeisureActivity
+import com.uszkaisandor.bored.views.LeisureActivityListItem
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun FavouriteActivitiesList(
+    pagingItems: LazyPagingItems<LeisureActivity>,
+    onFavouriteChecked: (String) -> Unit
+) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        state = rememberLazyListState(),
+        contentPadding = PaddingValues(20.dp)
+    ) {
+        items(
+            count = pagingItems.itemCount,
+            key = pagingItems.itemKey { it.id },
+        ) { index ->
+            pagingItems[index]?.let {
+                LeisureActivityListItem(
+                    modifier = Modifier.animateItemPlacement(
+                        animationSpec = tween(
+                            durationMillis = 200,
+                            easing = FastOutLinearInEasing
+                        )
+                    ),
+                    leisureActivity = it,
+                    onDeleteClicked = onFavouriteChecked
+                )
+            }
+        }
+        item {
+            if (pagingItems.loadState.append is LoadState.Loading) {
+                CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+            }
+        }
+    }
+}
