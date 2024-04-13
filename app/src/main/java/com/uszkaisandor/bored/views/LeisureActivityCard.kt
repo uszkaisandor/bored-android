69197c1e645c3b3be6e204196f07bd8116e3e68f
Sándor Uszkai <uszkaisanyi@gmail.com>
2024-04-13 09:35:47 +0200

    Revamp LeisureActivityCard

--- app/src/main/java/com/uszkaisandor/bored/views/LeisureActivityCard.kt
+++ app/src/main/java/com/uszkaisandor/bored/views/LeisureActivityCard.kt
@@ -2,16 +2,15 @@ package com.uszkaisandor.bored.views
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
@@ -20,19 +19,30 @@ import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.uszkaisandor.bored.R
 import com.uszkaisandor.bored.domain.LeisureActivity
 import com.uszkaisandor.bored.domain.toColor
 import com.uszkaisandor.bored.domain.toEmoji
+import com.uszkaisandor.bored.domain.toImageUrl
+import com.uszkaisandor.bored.domain.toStringResource
 import com.uszkaisandor.bored.presentation.common.shimmerBrush
 import com.uszkaisandor.bored.presentation.favourites.FavouriteButton
 import com.uszkaisandor.bored.presentation.home.sampleLeisureActivity
@@ -57,57 +67,73 @@ fun LeisureActivityCard(
                     color = MaterialTheme.colorScheme.surfaceContainer,
                     shape = RoundedCornerShape(16.dp)
                 )
-                .padding(all = 16.dp)
+                .padding(bottom = 16.dp)
         ) {
             val colors = ExtendedTheme.colors
             val accentColor = leisureActivity.type.toColor(colors)
+            var showShimmer by remember { mutableStateOf(true) }
+
             Box(
                 modifier = Modifier
-                    .height((3 * 28 + 16).dp)
+                    .wrapContentHeight()
                     .fillMaxWidth()
             ) {
-                Text(
-                    text = leisureActivity.name,
+                AsyncImage(
+                    model = leisureActivity.type.toImageUrl(),
                     modifier = Modifier
-                        .align(Alignment.Center)
-                        .animateContentSize()
-                        .padding(horizontal = 16.dp),
-                    textAlign = TextAlign.Center,
-                    style = Typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onSurface
+                        .fillMaxWidth()
+                        .alpha(0.7f)
+                        .aspectRatio(2f)
+                        .background(
+                            shimmerBrush(
+                                targetValue = 1200f,
+                                showShimmer = showShimmer
+                            )
+                        )
+                        .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    onSuccess = { showShimmer = false }
                 )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .background(color = accentColor, shape = CircleShape)
-                        .padding(8.dp),
-                    text = leisureActivity.type.toEmoji(),
-                    style = MaterialTheme.typography.headlineSmall,
-                )
-
-                Spacer(modifier = Modifier.width(12.dp))
 
                 FavouriteButton(
                     modifier = Modifier
-                        .size(50.dp)
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.surface),
+                        .size(48.dp)
+                        .clip(RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp))
+                        .align(Alignment.TopEnd)
+                        .background(MaterialTheme.colorScheme.surfaceContainer.copy(alpha = 0.7f)),
                     isFavourite = leisureActivity.isFavourite,
                     onClicked = { isChecked ->
                         onFavouriteChecked(isChecked)
                     }
                 )
+
+                Text(
+                    text = leisureActivity.name,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .animateContentSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    MaterialTheme.colorScheme.surfaceContainer,
+                                    MaterialTheme.colorScheme.surface
+                                ),
+                            ),
+                            alpha = 0.8f
+                        )
+                        .padding(16.dp),
+                    textAlign = TextAlign.Center,
+                    style = Typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
             }
 
-            Spacer(modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
             SliderWithTitle(
+                modifier = Modifier.padding(horizontal = 16.dp),
                 title = stringResource(id = R.string.price_range),
                 value = leisureActivity.priceRange,
                 range = (0f..1f),
@@ -117,6 +143,7 @@ fun LeisureActivityCard(
             )
 
             SliderWithTitle(
+                modifier = Modifier.padding(horizontal = 16.dp),
                 title = stringResource(id = R.string.accessibility),
                 value = kotlin.math.abs(1 - leisureActivity.accessibility),
                 range = (0f..1f),
@@ -128,9 +155,40 @@ fun LeisureActivityCard(
             PersonsView(
                 persons = leisureActivity.participants,
                 modifier = Modifier
-                    .padding(bottom = 12.dp)
+                    .padding(bottom = 12.dp, start = 16.dp, end = 16.dp)
                     .fillMaxWidth()
             )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .wrapContentHeight(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(
+                        id = R.string.category,
+                        stringResource(id = leisureActivity.type.toStringResource()),
+                    ),
+                    style = MaterialTheme.typography.titleSmall
+                )
+                Text(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(start = 8.dp)
+                        .background(
+                            color = accentColor.copy(alpha = 0.7f),
+                            shape = CircleShape
+                        )
+                        .padding(4.dp),
+                    text = leisureActivity.type.toEmoji(),
+                    style = MaterialTheme.typography.labelMedium,
+                )
+            }
+
             leisureActivity.link?.let {
                 Spacer(modifier = Modifier.height(8.dp))
                 ClickableText(
